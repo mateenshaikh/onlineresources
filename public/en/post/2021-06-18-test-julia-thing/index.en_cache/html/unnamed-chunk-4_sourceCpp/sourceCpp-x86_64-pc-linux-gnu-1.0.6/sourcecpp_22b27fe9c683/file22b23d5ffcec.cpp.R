4ca1707ce9bc7fe0c2bd3fe32a5181b8805732a8
Mateen Shaikh <m2shaikh@gmail.com>
2021-06-18 20:00:48 -0700

    first instantiation

---
+++ public/en/post/2021-06-18-test-julia-thing/index.en_cache/html/unnamed-chunk-4_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.6/sourcecpp_22b27fe9c683/file22b23d5ffcec.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_3_DLLInfo` <- dyn.load('/home/mateen/Documents/github/netlifyplayground/learn/content/post/2021-06-18-test-julia-thing/index.en_cache/html/unnamed-chunk-4_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.6/sourcecpp_22b27fe9c683/sourceCpp_4.so')
+
+getroot_cpp <- Rcpp:::sourceCppFunction(function() {}, FALSE, `.sourceCpp_3_DLLInfo`, 'sourceCpp_3_getroot_cpp')
+
+rm(`.sourceCpp_3_DLLInfo`)
